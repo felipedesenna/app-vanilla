72d2d4ac9c79f8f6dc081bb978aecb209d5c9f18
Wallace Nascimento Pereira <wallace.pereira@semantix.com.br>
2020-08-27 23:15:23 -0300

    ajustes

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,16 @@
-'use Strict';
 window.document.addEventListener('readystatechange', function (e) {
   e.preventDefault();
   var { target: doc } = e;
   var formAddTask = $('#add-task');
   var allTasks = $('.all-tasks');
-  var total = $('.app-calc');
+  var addButton = $('.app-button');
+  var appCalc = $('.app-calc');
+  var appInput = $('.app-input');
+  var alert = $('.app-alert');
   var footerContent = $('.footer-content');
   var tasksDuration = [];
   var tasksDispatchButton = [];
+  var isRunning = [];
   var today = new Date();
   today.setHours(0);
   today.setMinutes(0);
@@ -18,69 +21,75 @@ window.document.addEventListener('readystatechange', function (e) {
   var intervalEvents = [];
 
   formAddTask.addEventListener('submit', InsertTask);
-  total.addEventListener('click', calcTotal);
+  appCalc.addEventListener('click', calcTotal);
 
   //events
 
   function InsertTask(evt) {
     evt.preventDefault();
-    var name = evt.target[0].value;
-    var duration = today;
+    var name = this.newTask.value;
+    var duration = '';
     var id = new Date().getTime();
 
     if (name.length && name.length >= 5) {
-      addTask({ id, name, duration });
-      renderTask();
-      InitFields();
+      var hasTask = _tasks.find((task) => task.name === name);
+      var timer;
+      clearInterval(timer);
+
+      if (!hasTask) {
+        clearField(this.newTask);
+        addTask({ id, name, duration });
+        renderTask();
+        InitFields();
+        return;
+      }
+
+      alert.classList.add('show');
+      timer = setTimeout(function () {
+        alert.classList.remove('show');
+      }, 2000);
+      return;
     }
   }
 
   function runStop(i) {
     return function (evt) {
       const { checked } = evt.target;
-      console.log(i, checked);
-      var { id, name, duration } = _tasks[i];
-
+      var { id } = _tasks[i];
+      var time = new Date(today);
+      var taskTime = $(`#taskDur-${id}`);
+      var prevTime = timeValue(taskTime.value);
+      isRunning[i] = checked;
+      blockButtons(isRunning);
       if (!checked) {
         clearInterval(intervalEvents[i]);
         return;
       }
 
       intervalEvents[i] = setInterval(() => {
-        var elem = document.getElementById(`taskDur-${id}`);
-        var [hh, mm, ss] = elem.value.split(':');
-
-        document.getElementById(`taskDur-${id}`).value = timer(
-          Number(hh),
-          Number(mm),
-          Number(ss)
-        );
-      }, 100);
+        time.setSeconds(prevTime++);
+        var timeFormat = time.toLocaleTimeString('pt-BR');
+        _tasks[i].duration = timeFormat;
+        taskTime.value = timeFormat;
+      }, 1000);
     };
   }
 
-  var timer = (hh = 0, mm = 0, ss = 0) => {
-    ss = ss + 1;
+  function calcTotal(evt) {
+    var totalTime = [].map.call($$('.app-input-time'), function (
+      currentValue,
+      index,
+      collection
+    ) {
+      return currentValue.value;
+    });
 
-    if (ss === 59) {
-      ss = 0;
-      mm = mm + 1;
+    var sumTime = totalTime.reduce((acc, value) => {
+      return formatTime(timeValue(acc) + timeValue(value));
+    });
 
-      if (mm === 59) {
-        mm = 0;
-        hh = hh + 1;
-      }
-    }
-
-    let format =
-      (hh < 10 ? '0' + hh : hh) +
-      ':' +
-      (mm < 10 ? '0' + mm : mm) +
-      ':' +
-      (ss < 10 ? '0' + ss : ss);
-
-    return format;
-  };
+    $('.app-total').value = sumTime;
+  }
 
   // utils
 
@@ -94,6 +103,11 @@ window.document.addEventListener('readystatechange', function (e) {
 
   function timeStampConvert(val) {
     var date = new Date(today);
+    if (!!val) {
+      var seconds = timeValue(val);
+      date.setSeconds(seconds);
+    }
+
     return date.toLocaleTimeString('pt-BR');
   }
 
@@ -129,47 +143,30 @@ window.document.addEventListener('readystatechange', function (e) {
       footerContent.classList.add('animate');
       allTasks.innerHTML = render;
       tasksDuration = $$('.app-input-duration');
-
       tasksDispatchButton = $$('.task-button-dispatch');
     }
   }
 
   function timeValue(time) {
-    var item = time.split(":");
-    
-    return (item[0] * 3600) +
-           (item[1] * 60) +
-           (+item[2]);
+    var item = time.split(':');
+
+    return item[0] * 3600 + item[1] * 60 + item[2] * 1;
   }
-  
+
   function padHour(num) {
-    if(num < 10) {
-      return "0" + num;
+    if (num < 10) {
+      return '0' + num;
     } else {
-      return "" + num;
+      return '' + num;
     }
   }
-  
+
   function formatTime(seconds) {
-    return [padHour(Math.floor(seconds/3600)),
-            padHour(Math.floor(seconds/60)%60),
-            padHour(seconds%60),
-            ].join(":");
-  }
-
-  function calcTotal(evt) {
-    var totalTime = [].map.call(
-      document.getElementsByClassName('app-input-time'),
-      function (currentValue, index, collection) {
-        return currentValue.value;
-      }
-    );
-
-    var sumTime = totalTime.reduce((acc, value) => {
-      return formatTime(timeValue(acc) + timeValue(value));
-    });
-
-    $('.app-total').value = sumTime;
+    return [
+      padHour(Math.floor(seconds / 3600)),
+      padHour(Math.floor(seconds / 60) % 60),
+      padHour(seconds % 60),
+    ].join(':');
   }
 
   function InitFields() {
@@ -178,5 +175,22 @@ window.document.addEventListener('readystatechange', function (e) {
         tasksDispatchButton[i].addEventListener('change', runStop(i));
       }
     }
+  }
+
+  function blockButtons(arr) {
+    var timeRun = arr.find((i) => !!i);
+    if (!!timeRun) {
+      addButton.setAttribute('disabled', timeRun);
+      appCalc.setAttribute('disabled', timeRun);
+      return;
+    }
+    addButton.removeAttribute('disabled');
+    appCalc.removeAttribute('disabled');
+    return;
+  }
+
+  function clearField(field) {
+    field.value = '';
+    return;
   }
 });
